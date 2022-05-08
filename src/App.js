a4a912a2030dbe798a8aa9dcde481adc40bf781c
Sudipto Bose <sudipto.bose23@gmail.com>
2022-05-08 19:29:41 -0400

    added button to test api calls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Axios from 'axios';
+
 
 function App() {
+  
+  async function getData() {
+    await Axios.get(process.env.REACT_APP_API_LOCATION).then(
+      (response) => {
+        console.log(response.data);
+      })
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Champify: the cool app
+        <button onClick={getData}>test api call </button>
       </header>
     </div>
   );
