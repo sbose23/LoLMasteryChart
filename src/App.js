aed3aca66f33c4087a4880f173f2b0c27f6caea4
Sudipto Bose <sudipto.bose23@gmail.com>
2022-05-08 20:45:58 -0400

    attempt to fix server problem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-//import Axios from 'axios';
+import Axios from 'axios';
 
 
 function App() {
-  /*console.log("the begining")
+  console.log("the begining")
   async function getData() {
     await Axios.get(process.env.REACT_APP_API_LOCATION).then(
       (response) => {
         console.log(response.data);
       }).catch(err => console.error(err));
   }
-  <button onClick={getData}>test api call </button>*/
 
   
   return (
     <div className="App">
       <header className="App-header">
           Champify: the cool app
-        
+        <button onClick={getData}>test api call </button>
       </header>
     </div>
   );
