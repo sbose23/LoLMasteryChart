7d00d7695f3756b2db4098a7a0b4d5bf7d288685
Sudipto Bose <sudipto.bose23@gmail.com>
2022-05-10 10:15:31 -0400

    updated python script and json data to match needs

--- public/filterData.py
+++ public/filterData.py
@@ -1,21 +1,28 @@
 """
-this is a simple script I created to filter out useless data from the original json containing champion data
-the new json I create only maps a champion's number id to its champion id and name
-the champion id can be useful for getting champion photos as they use that name
-otherwise, there isnt much point in having them
+This is a simple script I created to filter out unecessary data from the official LoL json containing champion data.
+With this script I create two json files. One file maps champion key (given in api call) to champion name.
+The champion id (bit different from actual name in some cases) 
+can be useful for getting champion photos as they use the id instead of name. 
 """
 
 import json
 from urllib.request import urlopen
 
-new_data = {}
+keyToChampName = {}
+champNameToChampId = {}
 with urlopen("https://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/champion.json") as response:
   data = json.loads(response.read())
   for champion in data["data"]:
     championKey = data["data"][champion]["key"]
     championId = data["data"][champion]["id"]
     championName = data["data"][champion]["name"]
-    new_data[championKey] = {"champId": championId, "champName": championName}
 
-with open("public\championData.json", 'w') as f:
-  json.dump(new_data, f)
+    keyToChampName[championKey] = championName
+    champNameToChampId[championName] = championId
+
+with open("src\jsonData\keyToChampName.json", 'w') as f:
+  json.dump(keyToChampName, f)
+
+with open("src\jsonData\champNameToChampId.json", 'w') as f:
+  json.dump(champNameToChampId, f)
+
