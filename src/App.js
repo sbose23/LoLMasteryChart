b3f5f3d65062f03cb6593ef794c608812e9b4763
Sudipto Bose <sudipto.bose23@gmail.com>
2022-05-15 10:45:05 -0400

    updated homepage

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ function App() {
 
   return (
     <div className="App">
-      <br></br>
-      <br></br>
       <h4>LoLMasteryChart: A League of Legends Champion Mastery Visualizer </h4>
       <br></br>
       <Form data={data} setData={setData} />
       <Graph data={data} />
       <br></br>
       <br></br>
-      <a href="https://github.com/sbose23/LoLMasteryChart"> Source Code on GitHub</a>
+      <a href="https://github.com/sbose23/LoLMasteryChart" target="blank"> Source Code on GitHub</a>
       <small>LoLMasteryChart isn't endorsed by Riot Games and doesn't reflect the views or opinions of 
         Riot Games or anyone officially involved in producing or managing Riot Games properties. 
         Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</small>
