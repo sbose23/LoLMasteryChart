b085f265467566a632d5c6eb5694e1b65cdb103e
Sudipto Bose <sudipto.bose23@gmail.com>
2022-05-22 18:04:28 -0400

    added json file that contains champions by class

--- public/filterData.py
+++ public/filterData.py
@@ -1,28 +1,34 @@
 """
 This is a simple script I created to filter out unecessary data from the official LoL json containing champion data.
 With this script I create two json files. One file maps champion key (given in api call) to champion name.
-The champion id (bit different from actual name in some cases) 
-can be useful for getting champion photos as they use the id instead of name. 
+The other arranges champions by their primary classes. 
 """
 
 import json
 from urllib.request import urlopen
 
 keyToChampName = {}
-champNameToChampId = {}
+champClassData = {"Fighter": [], "Tank": [], "Mage": [], "Assassin": [], "Marksman": [], "Support": []}
+
 with urlopen("https://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/champion.json") as response:
   data = json.loads(response.read())
-  for champion in data["data"]:
-    championKey = data["data"][champion]["key"]
-    championId = data["data"][champion]["id"]
-    championName = data["data"][champion]["name"]
+  for obj in data["data"]:
+    champion = data["data"][obj]
+
+    championKey = champion["key"]
+    championName = champion["name"]
+
+    championClass = champion["tags"][0]
+    champClassData[championClass].append(championName)
 
     keyToChampName[championKey] = championName
-    champNameToChampId[championName] = championId
 
 with open("src\jsonData\keyToChampName.json", 'w') as f:
   json.dump(keyToChampName, f)
 
-with open("src\jsonData\champNameToChampId.json", 'w') as f:
-  json.dump(champNameToChampId, f)
+
+with open("src\jsonData\champClassData.json", 'w') as f:
+  json.dump(champClassData, f)
+
+
 
